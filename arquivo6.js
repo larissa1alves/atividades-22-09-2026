fb2ef79465b007477581860ffcc7d445e5274136
larissa1alves <larissaalvespereirap@gmail.com>
2026-09-23 15:34:50 -0300

    Add files via upload

---
+++ arquivo6.js
@@ -0,0 +1,63 @@
+let pessoa = {
+
+nome=prompt( digite seu nome ),
+idade=prompt(digite sua idade ),
+profissao=prompt(digite sua profissao)
+
+}
+
+function formatarPessoa(pessoa) {
+    
+    return alert("óla, meu nome é +",tenho "+pessoa.idade+"anos trabalho como "+ pessoa.profisso)
+}
+formatarPessoa(pessoa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+6. Crie uma função chamada formatarPessoa que receba um objeto representando
+uma pessoa com as propriedades nome, idade e profissao. A função deve retornar
+uma frase formatada no padrão: "Olá, meu nome é [nome], tenho [idade] anos e
+trabalho como [profissao]."

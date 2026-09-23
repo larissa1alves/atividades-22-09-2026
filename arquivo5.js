f6a194175f30e53ebbeb2f9cfd3fbff967bb1309
larissa1alves <larissaalvespereirap@gmail.com>
2026-09-23 15:18:31 -0300

    Add files via upload

---
+++ arquivo5.js
@@ -0,0 +1,19 @@
+function somarElementos(vetor) {
+   let somaTotal = 0
+    vetor.forEach(element => {
+        somaTotal += element
+    })
+
+    return somarTotal
+}
+
+
+
+//os meninos me ajudaram a fazer 
+
+
+
+
+
+
+

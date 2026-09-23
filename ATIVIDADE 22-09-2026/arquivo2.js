ae7797d5a58dccd480833393c407ec8f50ab075a
larissa1alves <larissaalvespereirap@gmail.com>
2026-09-23 13:48:59 -0300

    Add files via upload

---
+++ ATIVIDADE 22-09-2026/arquivo2.js
@@ -0,0 +1,12 @@
+function ehPar{
+let num=number(prompt("digite o numero:"))
+let verificacao=num%2
+if(verificacao==0){
+    return verificacao==verificacao
+}else{
+    return verificacao!==verificacao
+}
+
+
+}
+alert(ehPar())

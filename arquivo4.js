88dc3ef86dbb009e0cb174f1e7b635cd90e1686a
larissa1alves <larissaalvespereirap@gmail.com>
2026-09-23 15:06:28 -0300

    Add files via upload

---
+++ arquivo4.js
@@ -0,0 +1,13 @@
+function  calcularIMC (kg,m){
+    let imc= peso /(altura*altura){
+         if (imc < 18.5) {
+        return "Abaixo do peso";
+    } else if (imc <= 24.9) {
+        return "Peso normal";
+    } else {
+        return "Sobrepeso";
+    }
+}
+    }
+
+//essa  questao eu fiz ela olhando as outras, ja meio que tinha uma nocao
